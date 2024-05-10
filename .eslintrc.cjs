710781c586f3cbdf79ef65a70e6d999cc3b847d0
BryanS1989 <bryanstevenal@gmail.om>
2024-05-10 17:46:40 +0200

    Prettier and Eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
@@ -11,4 +14,4 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest'
   }
-}
+};
