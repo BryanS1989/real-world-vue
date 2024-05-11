d434b74a24312adad07a979187d1ecf10a7ba9c6
BryanS1989 <bryanstevenal@gmail.om>
2024-05-11 14:34:05 +0200

    Programmatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,13 +1,61 @@
 <script setup>
+import { useRouter } from 'vue-router';
 const props = defineProps({
     event: {
         required: true,
     },
 });
+
+const router = useRouter();
+
+const register = () => {
+    // If registration API calls is successful push back to the event details
+
+    // Navigate the user to this location (Programmatic Navigation)
+    router.push({
+        name: 'event-details',
+        id: props.event.id,
+    });
+
+    /*
+        Other examples of Programmatic Navigation:
+
+        // Path as string
+        router.push('/about');
+
+        // Path with an object
+        router.push({ path: '/about' });
+
+        // Named path
+        router.push({ name: 'about' });
+
+        // Dynamic segment
+        router.push({ name: 'event-details', params: { id: 3 } });
+
+        // Query parameter
+        router.push({ name: 'event-list', query: { page: 2 } }); // Since event-list is at our root path this pushes to: /?page=2
+    */
+
+    /*
+        To Navigate without pushing a history entry into the browser (disabling tha back button):
+
+        router.replace({ name: 'event-details', params: { id: 3 } });
+
+        this will replace the current page with the event-details page
+    */
+
+    /*
+        You can programmatically invoke the browser back and forward buttons with:
+
+        router.go(1);   --> Goes forward 
+        router.go(-1);  --> Goes backward 
+    */
+};
 </script>
 
 <template>
     <p>Register for the event here!!!</p>
+    <button @click="register">Register Me!</button>
 </template>
 
 <style scoped></style>
